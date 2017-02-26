245e6e1265ed843d45bb3e94ea2bb1fbb8ecd63f
St-Preux <ty@Tylers-MacBook-Pro.local>
2017-02-26 01:06:05 -0500

    VR mangohacks 2017 project

---
+++ Assets/backButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class backButton : MonoBehaviour {
+
+	public Button back;
+	// Use this for initialization
+	void Start () {
+	
+	back = back.GetComponent<Button> ();
+
+	}
+
+	public void bioPress()
+	{
+		back.enabled = true;
+
+	}
+
+	public void backScene ()
+	{
+		SceneManager.LoadScene ("Cubes");
+
+	}
+
+	// Update is called once per frame
+
+}
+
---
+++ Assets/menuOptions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menuOptions : MonoBehaviour {
+
+	public Button bio;
+	// Use this for initialization
+	void Start () {
+	
+	bio = bio.GetComponent<Button> ();
+		
+	}
+	
+	public void bioPress()
+	{
+		bio.enabled = true;
+
+	}
+
+	public void bioScene ()
+	{
+		SceneManager.LoadScene ("Biology");
+
+
+
+	}
+
+
+}
